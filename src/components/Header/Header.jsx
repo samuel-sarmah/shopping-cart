51574955780fee0fda3f4d30ae586e535bb9ac4c
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-10-16 17:16:45 +0300

    Import styles and test in browser

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router'
 import classes from './header.module.scss'
+import logo from '../../assets/shopping-icon.svg'
 
 export default function Header() {
     return (
         <>
-
+            <div className={classes.header}>
+                    <img src={logo} alt="shopping cart Application" className='classes.logo'/>
+            </div>
         </>
     )
 }
