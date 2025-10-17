1c1ec5426ea7f6812a4ca7bc2f2d4e69e2c52e53
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-10-17 05:48:08 +0300

    Import cart widget to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import { Link } from 'react-router'
 import classes from './header.module.scss'
 import logo from '../../assets/shopping-icon.svg'
+import CartWidget from '../cartWidget/CartWidget'
 
 export default function Header() {
     return (
         <>
             <div className={classes.header}>
-                    <img src={logo} alt="shopping cart Application" className='classes.logo'/>
+                <div>
+                    <Link to="/">
+                        <img src={logo} alt="shopping cart Application" className={classes.logo}/>
+                    </Link>
+                </div>
+                <div>
+                    <CartWidget productsCount={0} />
+                </div>
             </div>
+            
         </>
     )
 }
