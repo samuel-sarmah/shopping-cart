e09913a2a9d35e89831eb26c3e35f220b8e4ae1c
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-10-18 03:01:17 +0300

    Pass products count as props

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,37 @@ import { Link } from 'react-router'
 import classes from './header.module.scss'
 import logo from '../../assets/shopping-icon.svg'
 import CartWidget from '../cartWidget/CartWidget'
+import { useEffect, useState } from 'react'
 
-export default function Header() {
+export default function Header({ cartItemsCount }) {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const DISTANCE_FROM_TOP = 140;
+            const scrollY = window.scrollY || document.documentElement.scrollTop;
+            setIsScrolled(scrollY > DISTANCE_FROM_TOP);
+        }
+        window.addEventListener("scroll", handleScroll, false)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
     return (
         <>
-            <div className={classes.header}>
+            <div className={`${classes.header} ${isScrolled ? classes.shrink : ''}`}>
                 <div>
                     <Link to="/">
                         <img src={logo} alt="shopping cart Application" className={classes.logo}/>
                     </Link>
                 </div>
+                <h1>
+                    Eesto shoppers
+                </h1>
                 <div>
-                    <CartWidget productsCount={0} />
+                    <CartWidget productsCount={cartItemsCount || 0} />
                 </div>
             </div>
             
