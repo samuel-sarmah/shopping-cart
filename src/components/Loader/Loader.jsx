98884d1d8da88d2564be063dc0b4688dabc509fb
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-10-27 12:35:20 +0200

    feat: add Loader component for loading states
    - Create reusable Loader component with spinning animation
    - Add centered fullscreen loader layout
    - Implement smooth CSS spin animation (1s linear infinite)
    - Style with subtle gray spinner with darker top accent

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,7 @@
+import classes from './loader.module.scss'
+
+export default function Loader() {
+    <div className={classes.loader}>
+        <div className={classes.spinner}></div>
+    </div>
+}
